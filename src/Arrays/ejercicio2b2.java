4462f9c814e08e70a94282d5e70b46c780497b55
diurno <diurno@pc-aula>
2018-10-18 14:57:10 +0200

    sss

--- src/Arrays/ejercicio2b2.java
+++ src/Arrays/ejercicio2b2.java
@@ -7,22 +7,22 @@ public class ejercicio2b2 {
 		int array2[] = new int [150];
 		int array3[] = new int [150];
 		
-		System.out.println("Primer array:\n");
+		System.out.println("Primer array:");
 		
 		for (int i = 0; i < array1.length; i++) {
 			
 			array1[i] =  (int)(Math.round(Math.random()*100));
-			System.out.println("Número " + i + ": " + array1[i]);
+			System.out.print(array1[i] + " ");
 		}
 		
-		System.out.println("\nSegundo array:\n");
+		System.out.println("\n\nSegundo array:");
 		
 		for (int i = 0; i < array2.length; i++) {
 			array2[i] =  (int)(Math.round(Math.random()*100));
-			System.out.println("Número " + i + ": " + array2[i]);
+			System.out.print(array2[i] + " ");
 		}
 		
-		System.out.println("\nTercer array:\n");
+		System.out.println("\n\nTercer array:");
 		
 		for (int i = 0; i < array3.length; i++) {
 			
@@ -33,7 +33,7 @@ public class ejercicio2b2 {
 				array3[i] = array2[i];
 			}
 			
-			System.out.println("Número " + i + ": " + array3[i]);
+			System.out.print(array3[i] + " ");
 		}
 	}
 
--- src/Arrays/ejercicio3b2.java
+++ src/Arrays/ejercicio3b2.java
@@ -5,15 +5,15 @@ public class ejercicio3b2 {
 	public static void main(String[] args) {
 		int numeros[] = new int [150], numusuario, resultado;
 		
-		numusuario = Integer.parseInt(JOptionPane.showInputDialog("Introduce el número por el cual quieres multiplicar el array"));
+		numusuario = Integer.parseInt(JOptionPane.showInputDialog("Introduce el nÃºmero por el cual quieres multiplicar el array"));
 		
 		for (int i = 0; i < numeros.length; i++) {
 			
 			numeros[i] = ((int) Math.round(Math.random() * 100));
 			resultado = (numeros[i] * numusuario);
 			
-			System.out.println("Número " + i + ": " + numeros[i] +
-					"\n Multiplicación: " + numeros[i] + " * " + numusuario + " = " + resultado);
+			System.out.println("NÃºmero " + i + ": " + numeros[i] +
+					"\n MultiplicaciÃ³n: " + numeros[i] + " * " + numusuario + " = " + resultado);
 			
 		}
 
