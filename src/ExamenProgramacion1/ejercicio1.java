7db44aa343b4ac97cd2b01455317aee6511919de
diurno <diurno@pc-aula>
2018-10-30 13:59:20 +0100

    kk

--- src/ExamenProgramacion1/ejercicio1.java
+++ src/ExamenProgramacion1/ejercicio1.java
@@ -8,49 +8,51 @@ public class ejercicio1 {
 		int opcion, num1, num2, resultado;
 		
 		do {
-			opcion = Integer.parseInt(JOptionPane.showInputDialog("Elige una opción:" +
-					 "\n1-. Calcular área de un triángulo rectángulo." +
-					 "\n2-. Perímetro de una circunferencia." +
-					 "\n3-. Generar números al azar entre 0 y 100 hasta que salga 47." +
+			opcion = Integer.parseInt(JOptionPane.showInputDialog("Elige una opciï¿½n:" +
+					 "\n1-. Calcular ï¿½rea de un triï¿½ngulo rectï¿½ngulo." +
+					 "\n2-. Perï¿½metro de una circunferencia." +
+					 "\n3-. Generar nï¿½meros al azar entre 0 y 100 hasta que salga 47." +
 					 "\n0-. Salir. "));
 			
 			if (opcion < 0 || opcion > 3) {
-				JOptionPane.showMessageDialog(null, "Por favor, introduce una opción válida.");
+				JOptionPane.showMessageDialog(null, "Por favor, introduce una opciï¿½n vï¿½lida.");
 			}
+			else {
 	
-			switch (opcion) {
-			
-				case 0:{
-					break;
-				}
+				switch (opcion) {
 				
-				case 1:{
-					num1 = Integer.parseInt(JOptionPane.showInputDialog("Introduce la base del triángulo:"));
-					num2 = Integer.parseInt(JOptionPane.showInputDialog("Introduce la altura del triángulo:"));
-					resultado = (num1 * num2)/2;
+					case 0:{
+						break;
+					}
 					
-					JOptionPane.showMessageDialog(null, "El área del triángulo es " + resultado);
-					break;
-				}
-				
-				case 2:{
-					num1 = Integer.parseInt(JOptionPane.showInputDialog("Introduce el radio de la circunferencia:"));
-					resultado = (int) ( Math.PI * (Math.pow(num1, 2)));
+					case 1:{
+						num1 = Integer.parseInt(JOptionPane.showInputDialog("Introduce la base del triï¿½ngulo:"));
+						num2 = Integer.parseInt(JOptionPane.showInputDialog("Introduce la altura del triï¿½ngulo:"));
+						resultado = (num1 * num2)/2;
+						
+						JOptionPane.showMessageDialog(null, "El ï¿½rea del triï¿½ngulo es " + resultado);
+						break;
+					}
 					
-					JOptionPane.showMessageDialog(null, "El perímetro de la circunferencia es " + resultado);
-					break;
-				}
-				
-				case 3:{
-					do {
-						num1 = ((int) Math.round(Math.random() * 100));
-						JOptionPane.showMessageDialog(null, num1);
-					} while (num1 != 47);
-					break;
-				}
+					case 2:{
+						num1 = Integer.parseInt(JOptionPane.showInputDialog("Introduce el radio de la circunferencia:"));
+						resultado = (int) ( Math.PI * (Math.pow(num1, 2)));
+						
+						JOptionPane.showMessageDialog(null, "El perï¿½metro de la circunferencia es " + resultado);
+						break;
+					}
+					
+					case 3:{
+						do {
+							num1 = ((int) Math.round(Math.random() * 100));
+							JOptionPane.showMessageDialog(null, num1);
+						} while (num1 != 47);
+						break;
+					}
+				} 
+			}
 			
-			} 
-		} while (opcion > 0 || opcion < 3);
+		} while (opcion != 0 );
 		
 	}
 
